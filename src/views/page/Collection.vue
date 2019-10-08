435e0fe9c3bd9e04ade1725c32f07c05ba2e31a9
HackerBoy <admin@hackerboy.com>
2019-10-08 11:05:39 +0700

    pages

---
+++ src/views/page/Collection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="pages">
+    <loader v-if="isLoading" />
+
+    <template v-else>
+      <h1>Pages</h1>
+      <hr />
+
+      <template v-if="pagesJsonApi">
+        <div class="row mb-3" v-for="page in pagesJsonApi.document.data">
+          <div class="col-12">
+            <h3><router-link :to="{ name: 'page.detail', params: { slug: page.attributes.slug } }">{{ page.attributes.title }}</router-link></h3>
+            <p>{{ page.attributes.description }}</p>
+          </div>
+        </div>
+        <Pagination :number-of-results="pagesJsonApi.document.data.length" :results-per-page="pageLimit" />
+      </template>
+
+    </template>
+    
+  </div>
+</template>
+
+<script type="text/javascript">
+import base from 'lite-store-vue-base';
+
+import Pagination from '@/components/Pagination';
+
+export default {
+
+  mixins: [base.mixins.page.collection],
+
+  components: { Pagination }
+
+}
+</script>
---
+++ src/views/page/Detail.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="page-detail">
+    
+    <loader v-if="isLoading" />
+
+    <template v-if="pageJsonApi">
+      
+      <h1>{{ pageJsonApi.document.data.attributes.title }}</h1>
+      <p>{{ pageJsonApi.document.data.attributes.description }}</p>
+      <hr />
+
+      <article v-html="pageJsonApi.document.data.attributes.content"></article>
+
+    </template>
+
+    <ContactForm v-if="pageJsonApi && pageJsonApi.document.data.attributes.show_contact_form" />
+
+  </div>
+</template>
+
+<script type="text/javascript">
+import base from 'lite-store-vue-base';
+import ContactForm from 'lite-store-vue-base/views/components/ContactForm';
+
+export default {
+
+  mixins: [base.mixins.page.detail],
+
+  components: { ContactForm }
+
+}
+</script>
+
+<style type="text/css">
+
+.hiweb-contact-form input, .hiweb-contact-form textarea {
+  margin: 0;
+  max-width: 100%;
+  -webkit-box-flex: 1;
+  -ms-flex: 1 0 auto;
+  flex: 1 0 auto;
+  outline: 0;
+  -webkit-tap-highlight-color: rgba(255,255,255,0);
+  text-align: left;
+  line-height: 1.21428571em;
+  padding: .67857143em 1em;
+  background: #fff;
+  border: 1px solid rgba(34,36,38,.15);
+  color: rgba(0,0,0,.87);
+  border-radius: .28571429rem;
+  -webkit-transition: border-color .1s ease,-webkit-box-shadow .1s ease;
+  transition: border-color .1s ease,-webkit-box-shadow .1s ease;
+  transition: box-shadow .1s ease,border-color .1s ease;
+  transition: box-shadow .1s ease,border-color .1s ease,-webkit-box-shadow .1s ease;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+
+.hiweb-contact-form__heading {
+  padding: 0px 10px; 
+}
+
+.hiweb-contact-form__form__input, .hiweb-contact-form__form__errors, .hiweb-contact-form__form__success, .hiweb-contact-form__form__submit {
+  display: inline-block;
+  width: 50%;
+  padding: 10px;
+}
+
+.hiweb-contact-form__form__input--message, .hiweb-contact-form__form__input input, .hiweb-contact-form__form__input textarea, .hiweb-contact-form__form__errors, .hiweb-contact-form__form__success {
+  width: 100%;
+}
+
+.hiweb-contact-form__form__input textarea {
+  height: 180px;
+}
+
+.hiweb-contact-form__form__submit__button {
+  background-color: #f5f5f5;
+  border-color: transparent;
+  border-width: 1px;
+  color: #363636;
+  cursor: pointer;
+  justify-content: center;
+  padding-bottom: calc(.375em - 1px);
+  padding: .75em;
+  text-align: center;
+  white-space: nowrap;
+}
+</style>
