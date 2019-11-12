026cc19be13cda9d875ebdda9d978b6df685cb2f
nguyen minh quy <hanhgiacodoc9x@gmail.com>
2019-11-12 09:47:53 +0700

    update  upsell page

--- src/views/upsell/Index.vue
+++ src/views/upsell/Index.vue
@@ -1,17 +1,102 @@
 <template>
   <div class="upsell">
 
-    <div class="row align-items-center">
+    <div class="upsell--top row align-items-center">
 
       <!-- count down -->
-      <div class="col-sm-5">
-        <div class="upsell--top">
-          <div class="upsell--countdown">--:--:--</div>
+      <div class="col-sm-5 pb-3">
+        <div class="upsell--countdown">
+          <div class='upsell--countdown__item'>--<br/>Hours </div>
+          <div class='upsell--countdown__item'>--<br/>Minute </div>
+          <div class='upsell--countdown__item'>--<br/>Seconds </div>
         </div>
       </div>
 
-      <div class="col-sm-7 font-bold text-sm-right">
+      <div class="col-sm-7 font-bold text-sm-right pb-3">
         <h5><strong>Act Fast: This offer is not available elsewhere on our store!</strong></h5>
+      </div>
+      
+    </div>
+
+    <hr class="border-light" />
+
+    <div class="py-4">
+
+      <h4 class="text-center">LAST STEP: FIRST-CUSTOMER SPECIAL GIFT!</h4>
+
+      <div class="progress position-relative">
+        <div class="position-absolute bg-transparent" style="left: 50%; transform: translateX(-50%);">75% Complete</div>
+        <div class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar" style="width: 75%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+
+    </div>
+
+    <hr class="border-light" />
+
+    <div class="py-3 px-2 text-center" style="background: #efefef">
+
+      <h2 class="h1"><strong>Wait <span class="text-red">Hoshino</span>! Do You Want to Get Another Dragonfly Blanket at <span class="text-red">70% OFF?</span></strong></h2>
+
+      <h3><strong>You'll Never See This Offer Again!</strong></h3>
+      
+    </div>
+
+    <div class="text-center py-5 px-sm-5">
+
+      <h3><strong>Pick ONE of These Quilts and Get Instant <span class="text-red">70% OFF!</span></strong></h3>
+
+      <p class="h5 px-sm-5">This offer is a special gift for our <strong>First-Time Customer.</strong> Thank you for supporting us and becoming our awesome customer :)</p>
+      
+    </div>
+
+    <!-- product -->
+    <div class="row upsell__product">
+
+      <div v-for="i in 2" class="col-sm-6">
+
+        <div class="border rounded p-4 text-center upsell__product--content">
+
+          <p class="h5 text-uppercase">get this quilt at 70% off</p>
+
+          <img class="pt-4" src="https://images.unsplash.com/photo-1571419816020-fcb3d260164d?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60">
+
+          <h5 class="pt-3 pb-4"><strong>Dragonfly TL060522 Quilt Blanket</strong></h5>
+
+          <ul class="list-unstyled">
+            <li><i class="fas fa-circle pr-3" style="font-size: 6px"></i> <strong>Handmade Item and Made To Order!</strong></li>
+            <li><i class="fas fa-circle pr-3" style="font-size: 6px"></i> <strong>Handmade Item and Made To Order!</strong></li>
+            <li><i class="fas fa-circle pr-3" style="font-size: 6px"></i> <strong>Five Size Available:</strong> Throw, Twin, Queen, King and California/Super King!</li>
+            <li><i class="fas fa-circle pr-3" style="font-size: 6px"></i> <strong>Made with Kona Cotton,</strong> a little bit microfiber and <strong>so much love</strong> ?</li>
+          </ul>
+
+          <div class="py-3">
+            <span class="h4 mr-2 text-grey" style="text-decoration: line-through;">$139.90</span>
+            <span class="h4">$41.90</span>
+          </div>
+
+          <p class="h3 text-red">70% OFF</p>
+
+          <div class="py-2 row justify-content-center upsell__product--size">
+            <div class="col-sm-6">
+              <div class="text-left text-muted">Size:</div>
+              <select class="w-100 text-muted" style="outline: none">
+                <option>Queen</option>
+              </select>
+              <i class="upsell__product--size__icon fa fa-chevron-down float-right"></i>
+            </div>
+          </div>
+
+          <button class="btn bg-green text-white w-100 p-3 my-3">
+            <h4>Yes, Add To My Order</h4>
+            <span>We will ship it using DHL Express</span>
+          </button>
+
+          <img height="80px" src="@/assets/credit-card.png">
+
+          <p class="text-grey text-sm mt-3"><small>No thanks, I don't want to take this one-time offer for first-time customer.</small></p>
+          
+        </div>
+        
       </div>
       
     </div>
@@ -75,9 +160,49 @@ export default {
     display: inline-block;
     text-align: center;
     background: #ce3362;
-    min-width: 120px;
+    min-width: 100px;
     padding: 5px;
     color: white;
+    margin-right: 3px;
+  }
+
+  .text-red {
+    color: #fe2905;
+  }
+
+  .text-grey {
+    color: #a2a3a7;
+  }
+
+  .bg-green {
+    background: #28ac1e;
+  }
+
+  &__product {
+
+    &--content {
+      ul > li {
+        line-height: 28px;
+      }
+    }
+
+    &--size {
+
+      select {
+        background: transparent;
+        border-radius: 3px;
+        padding: 5px;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        -o-appearance: none;
+        appearance: none; 
+      }
+
+      &__icon {
+        margin-top: -25px;
+        margin-right: 8px;
+      }
+    }
   }
 
 }
